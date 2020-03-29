8ed0791591bd4de217f78e66b9bbbe286217c7d1
Vadim-rss <55102312+Vadim-rss@users.noreply.github.com>
2020-03-29 06:28:50 +0300

    add solution

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,32 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        var mlist = sadArray.toMutableList()
+
+        if(mlist.size < 2){
+            return sadArray
+        }
+
+        var isListHappy: Boolean = false
+        while (!isListHappy) {
+            isListHappy = true
+            for (i in 1 until mlist.size-1){
+
+                if (!checkIsHappy(mlist.get(i-1), mlist.get(i), mlist.get(i+1) ) ){
+                    mlist.removeAt(i)
+                    isListHappy = false
+                    break
+                }
+
+            }
+
+        }
+
+        return mlist.toIntArray()
+    }
+
+    private fun checkIsHappy(lElem: Int, cElem: Int, rElem: Int): Boolean {
+        return lElem + rElem > cElem
     }
 }
--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,51 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        var s1 = "" // []
+        var s2 = "" // <>
+        var s3 = "" // ()
+        var al = arrayListOf<String>()
+
+        inputString.forEachIndexed{i, ch ->
+
+            if (ch == '['){
+                s1 = inputString.substring(i+1)
+                s1 = s1.substring(0, findLastIndex(s1, ch, ']'))
+                println(s1)
+                al.add(s1)
+            }
+
+            if (ch == '('){
+                s2 = inputString.substring(i+1)
+                s2 = s2.substring(0, findLastIndex(s2, ch, ')'))
+
+                println(s2)
+                al.add(s2)
+            }
+
+            if (ch == '<'){
+                s3 = inputString.substring(i+1)
+                s3 = s3.substring(0, findLastIndex(s3, ch, '>'))
+                println(s3)
+                al.add(s3)
+            }
+        }
+
+
+        return al.toTypedArray()
+    }
+
+    fun findLastIndex(s: String, startChar: Char, lastChar: Char) : Int{
+        var cCounter = 0
+
+        for (j in s.indices) {
+            if (s[j] == startChar) cCounter++
+            if (s[j] == lastChar) {
+                if (cCounter == 0) return j
+                else cCounter--
+            }
+        }
+        return -1
     }
 }
